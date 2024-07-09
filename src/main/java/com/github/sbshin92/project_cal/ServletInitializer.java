f0c2f2875da046e035e1ea2e28a09f287d282e69
SBShin92 <aopaoi0987@gmail.com>
2024-07-09 10:27:29 +0900

    convert (jar -> war)

---
+++ src/main/java/com/github/sbshin92/project_cal/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.sbshin92.project_cal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectCalApplication.class);
+	}
+
+}
