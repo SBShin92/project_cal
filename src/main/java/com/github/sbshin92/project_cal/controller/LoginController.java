c32c740ddec6cef41d365862d7ba38f78bb88940
parkmj9515 <163090357+parkmj9515@users.noreply.github.com>
2024-07-11 09:41:31 +0900

    패스워드암호화 07.10

--- src/main/java/com/github/sbshin92/project_cal/controller/LoginController.java
+++ src/main/java/com/github/sbshin92/project_cal/controller/LoginController.java
@@ -2,17 +2,21 @@ package com.github.sbshin92.project_cal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+@RequestMapping("/login")
 @Controller
 public class LoginController {
 
-	@GetMapping("/login")
-	public String loginSuccess() {
-		return "login/login"; // login.jsp 페이지를 반환
-	}
+    @GetMapping
+    public String login() {
+        return "login/login"; // login.jsp 페이지를 반환
+    }
 
-	@GetMapping("/home")
-	public String home() {
-		return "home"; // 로그인 성공 후 홈 페이지로 리디렉션
-	}
+    @GetMapping("/home")
+    public String home() {
+        return "home"; // 홈 페이지를 반환
+    }
 }
