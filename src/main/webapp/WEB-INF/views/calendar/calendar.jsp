d6e4a1f9daa5aa1c1f88fc7b20549d84ad1a4c05
SBShin92 <aopaoi0987@gmail.com>
2024-07-12 13:01:51 +0900

    공휴일 업데이트, 버그 수정

--- src/main/webapp/WEB-INF/views/calendar/calendar.jsp
+++ src/main/webapp/WEB-INF/views/calendar/calendar.jsp
@@ -5,53 +5,104 @@
 <!DOCTYPE html>
 <html lang="ko">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href='<c:url value="/css/calendar.css" />' />
-    <title>OurCalendar</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link type="text/css" rel="stylesheet"
+	href='<c:url value="/css/calendar.css" />' />
+<title>OurCalendar</title>
 </head>
 <body>
-    <jsp:include page="/WEB-INF/includes/header.jsp" />
-    <jsp:include page="/WEB-INF/includes/nav.jsp" />
-    
-    <main>
-        <section class="calendar">
-            <table>
-                <thead>
-                    <tr>
-                        <th>일</th>
-                        <th>월</th>
-                        <th>화</th>
-                        <th>수</th>
-                        <th>목</th>
-                        <th>금</th>
-                        <th>토</th>
-                    </tr>
-                </thead>
-            </table>
-        </section>
-        <aside class="right-panel">
-       		<!-- 프로젝트 생성 버튼 추가 -->
-            <div class="create-project">
-                <a id="createProjectBtn" class="btn btn-primary" href="<c:url value='/project/create' />">프로젝트 생성</a>
-            </div>
-            <div class="project-list" id="projectList">
-                <h4>오늘의 프로젝트</h4>
-                
-                <c:if test="${ not empty projectListByDate }">
-                <c:forEach items="${ projectListByDate }" var="vo" varStatus="status">
-	                <div class="project-item">
-	                    <div class="project-title">
-	                    	<a href="<c:url value='/project' />/${vo.projectId}">${ vo.projectTitle }</a>
-	                    </div>
-	                    <div class="project-duration">${ vo.startDate } ~ ${ vo.endDate }</div>
-	                </div>
-                </c:forEach>
-                </c:if>
-            </div>
-        </aside>
-    </main>
-    
+	<jsp:include page="/WEB-INF/includes/header.jsp" />
+	<jsp:include page="/WEB-INF/includes/nav.jsp" />
+
+	<main>
+		<section class="calendar">
+			<table>
+				<thead>
+					<tr>
+						<th>일</th>
+						<th>월</th>
+						<th>화</th>
+						<th>수</th>
+						<th>목</th>
+						<th>금</th>
+						<th>토</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+
+				</tbody>
+			</table>
+		</section>
+		<aside class="right-panel">
+			<!-- 프로젝트 생성 버튼 추가 -->
+			<div class="create-project">
+				<a id="createProjectBtn" class="btn btn-primary"
+					href="<c:url value='/project/create' />">프로젝트 생성</a>
+			</div>
+			<div class="project-list" id="projectList">
+				<h4>오늘의 프로젝트</h4>
+
+				<c:if test="${ not empty projectListByDate }">
+					<c:forEach items="${ projectListByDate }" var="vo"
+						varStatus="status">
+						<div class="project-item">
+							<div class="project-title">
+								<a href="<c:url value='/project' />/${vo.projectId}">${ vo.projectTitle }</a>
+							</div>
+							<div class="project-duration">${ vo.startDate }~ ${ vo.endDate }</div>
+						</div>
+					</c:forEach>
+				</c:if>
+			</div>
+		</aside>
+	</main>
+
 	<script src="<c:url value='/javascript/calendar.js' />"></script>
 </body>
 </html>
--- src/main/webapp/javascript/calendar.js
+++ src/main/webapp/javascript/calendar.js
@@ -1,91 +1,155 @@
 const today = new Date();
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Get the current year and month
-    let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth() + 1;
-    
-    createCalendar(currentYear, currentMonth); // default: today's date
-    document.getElementsByClassName("prev-button")[0].addEventListener("click", () => {
-        currentMonth -= 1;
-        if (currentMonth === 0) {
-            currentMonth = 12;
-            currentYear -= 1;
-        }
-        createCalendar(currentYear, currentMonth);
-    });
-    document.getElementsByClassName("next-button")[0].addEventListener("click", () => {
-        currentMonth += 1;
-        if (currentMonth === 13) {
-            currentMonth = 1;
-            currentYear += 1;
-        }
-        createCalendar(currentYear, currentMonth);
-    });
+	// Get the current year and month
+	let currentYear = today.getFullYear();
+	let currentMonth = today.getMonth() + 1;
+
+	createCalendar(currentYear, currentMonth); // default: today's date
+
+	// 다음 달 버튼
+	document.getElementsByClassName("prev-button")[0].addEventListener("click", () => {
+		currentMonth -= 1;
+		if (currentMonth === 0) {
+			currentMonth = 12;
+			currentYear -= 1;
+		}
+		createCalendar(currentYear, currentMonth);
+	});
+
+	// 이전 달 버튼
+	document.getElementsByClassName("next-button")[0].addEventListener("click", () => {
+		currentMonth += 1;
+		if (currentMonth === 13) {
+			currentMonth = 1;
+			currentYear += 1;
+		}
+		createCalendar(currentYear, currentMonth);
+	});
 });
 
 
-const createCalendar = (year, month) => {
-    // 기존 tbody 제거
-    if (document.querySelector(".calendar table tbody") !== null)
-        document.querySelector(".calendar table tbody").remove();
 
-    // 지난 달 마지막 날짜, 요일
-    const lastMonthEndDate = new Date(year, month - 1, 0).getDate();
-    const lastMonthEndDay = new Date(year, month - 1, 0).getDay();
+const createCalendar = async (year, month) => {
+	// yearString, monthString
+	let yearString = year.toString();
+	let monthString = "";
+	if (month < 10)
+		monthString += "0";
+	monthString += month.toString();
 
-    // 이번 달 마지막 날짜, 요일
-    const currentMonthEndDate = new Date(year, month, 0).getDate();
-    const currentMonthEndDay = new Date(year, month, 0).getDay();
 
-    // 캘린더 tbody테이블 생성
-    let date = 1;
-    const tbody = document.createElement("tbody");
-    for (let i = 0; i < 6; i++) {
-        const tr = document.createElement("tr");
-        for (let j = 0; j < 7; j++) {
-            const td = document.createElement("td");
-            if (i === 0 && j <= lastMonthEndDay && lastMonthEndDay != 6) {
-                tr.appendChild(td);
-            } else if (date > currentMonthEndDate) {
-                tr.appendChild(td);
-            } else {
-                td.className = j === 0 || j === 6 ? "weekend" : "";
-                const divDate = document.createElement("div");
-                divDate.textContent = date;
-                td.appendChild(divDate);
+	// 지난 달 마지막 요일
+	const lastMonthEndDay = new Date(year, month - 1, 0).getDay();
 
-                td.addEventListener("click", () => {
-                    const clickedDateFormat = new Date(year, month, (i * 7) + j);
-                    window.location.href = "calendar/" + formatDate(clickedDateFormat)});
-                tr.appendChild(td);
-                date++;
-            }
-        }
-        tbody.appendChild(tr);
-        if (date > currentMonthEndDate)
-            break;
-    }
-    // tbody 쏴주기
-    document.querySelector(".calendar table").appendChild(tbody);
+	// 이번 달 마지막 날짜
+	const currentMonthEndDate = new Date(year, month, 0).getDate();
 
-    // 달력 날짜 갱신
-    document.getElementsByClassName("view-date")[0].textContent = `${year}년 ${month}월`;
+	// 년 월 공휴일 정보 받기
+	const holiday = await getHolidayMonth(yearString, monthString);
+
+	// 캘린더 tbody테이블 생성
+	let date = 1;
+	const tbody = document.createElement("tbody");
+	for (let i = 0; i < 6; i++) {
+		const tr = document.createElement("tr");
+		for (let j = 0; j < 7; j++) {
+			const td = document.createElement("td");
+			if (i === 0 && j <= lastMonthEndDay && lastMonthEndDay != 6) {
+				tr.appendChild(td);
+			} else if (date > currentMonthEndDate) {
+				tr.appendChild(td);
+			} else {
+				// 날짜를 문자열로 변환
+				let dateString = formatDate(new Date(year, month - 1, date));
+
+				// 주말이거나 공휴일이면 weekend 클래스 추가
+				td.className = (j === 0 || j === 6) ? "weekend" : "";
+				
+				// 날짜 숫자 표시 태그
+				const divDate = document.createElement("div");
+				divDate.textContent = date;
+
+				
+				if (holiday.some(h => h.locdate === dateString)) {
+					td.classList.add("weekend");
+					const holidayName = holiday.find(h => h.locdate === dateString).dateName;
+					divDate.textContent += ` (${holidayName})`;
+				}
+				(function(date) {
+					td.addEventListener("click", () => {
+						const clickedDateFormat = new Date(year, month - 1, date);
+						window.location.href = "calendar/" + formatDate(clickedDateFormat);
+					});
+				})(date);
+				td.appendChild(divDate);
+				tr.appendChild(td);
+				date++;
+			}
+		}
+		tbody.appendChild(tr);
+		if (date > currentMonthEndDate)
+			break;
+	}
+
+	// 기존 tbody 제거
+	const prevTbody = document.querySelector(".calendar table tbody");
+	if (prevTbody !== null)
+		prevTbody.remove();
+	// tbody 쏴주기
+	document.querySelector(".calendar table").appendChild(tbody);
+	// 달력 날짜 갱신
+	document.getElementsByClassName("view-date")[0].textContent = `${year}년 ${month}월`;
 };
 
 const formatDate = (date) => {
-    var d = new Date(date),
-        month = '' + (d.getMonth()),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+	returnYear = String(date.getFullYear());
+	returnMonth = String(date.getMonth() + 1);
+	returnDate = String(date.getDate());
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+	if (returnMonth.length < 2) 
+		returnMonth = '0' + returnMonth;
+	if (returnDate.length < 2)
+		returnDate = '0' + returnDate;
 
-    return [year, month, day].join('');
+	return [returnYear, returnMonth, returnDate].join('');
 }
+
+
+
+// 공휴일 체크
+const getHolidayMonth = async (year, month) => {
+	const holiday = [];
+	const url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo';
+	let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'sBPfuffF30IEOA0wWBrEhs6qPxJtvKZTsZFbjNT2SeEp1lTXBgGPES1b%2FxcnObIXvV4lQBxlRD0GI%2FLM%2Fb9GJQ%3D%3D';
+	queryParams += '&' + encodeURIComponent('solYear') + '=' + encodeURIComponent(year);
+	queryParams += '&' + encodeURIComponent('solMonth') + '=' + encodeURIComponent(month);
+
+	try {
+		const response = await fetch(url + queryParams);
+		if (!response.ok) {
+			console.error("공휴일 api를 불러오는 데 실패했습니다.");
+			return;
+		}
+		const text = await response.text();
+		const parser = new DOMParser();
+		const xmlDoc = parser.parseFromString(text, "text/xml");
+
+		const items = xmlDoc.getElementsByTagName("item");
+
+		if (items.length === 0)
+			return holiday;
+		for (let i = 0; i < items.length; i++) {
+			const locdate = items[i].getElementsByTagName("locdate")[0].textContent;
+			const dateName = items[i].getElementsByTagName("dateName")[0].textContent;
+			holiday.push({ "locdate": locdate, "dateName": dateName });
+		}
+	} catch (error) {
+		console.error(error);
+	}
+	return holiday;
+}
+
 
 
 
