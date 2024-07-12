9d931fc134b8925058d38eb63da5ca08696ff407
SBShin92 <aopaoi0987@gmail.com>
2024-07-12 17:27:07 +0900

    fix calendar(ajax -> redirect)

--- src/main/java/com/github/sbshin92/project_cal/controller/CalendarController.java
+++ src/main/java/com/github/sbshin92/project_cal/controller/CalendarController.java
@@ -2,7 +2,9 @@ package com.github.sbshin92.project_cal.controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -16,6 +18,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.github.sbshin92.project_cal.data.vo.ProjectVO;
 import com.github.sbshin92.project_cal.service.CalendarService;
 
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/calendar")
 @Controller
 public class CalendarController {
@@ -23,17 +27,45 @@ public class CalendarController {
 	private CalendarService calendarService;
 	
 	@GetMapping({"", "/"})
-	public String calendarPage() {
+	public String calendarPage(HttpSession session) {
+		if (session.getAttribute("viewMonth") == null && session.getAttribute("viewDate") == null) {
+			LocalDate localDate = LocalDate.now();
+			int year = localDate.getYear();
+			int month = localDate.getMonthValue();
+			session.setAttribute("viewYear", year);
+			session.setAttribute("viewMonth", month);
+		}
 		return "calendar/calendar";
 	}
 	
-	@GetMapping("/{date}")
-	public String calendarPageWithDateProjectList(@PathVariable("date") String dateString, 
-			RedirectAttributes redirectAttributes) {
-		Date date = stringToDate(dateString);
-		List<ProjectVO> lst = calendarService.getProjectListByDate(date);
+	@GetMapping("/{yearMonth}")
+	public String calendarPageWithMonth(@PathVariable("yearMonth") String yearMonth, HttpSession session) {
+		
+		try {
+			int year = Integer.parseInt(yearMonth.substring(0, 4));
+			int month = Integer.parseInt(yearMonth.substring(4));
+			if (month > 12 || month < 1)
+				month = 0;
+			session.setAttribute("viewYear", year);
+			session.setAttribute("viewMonth", month);			
+		} catch (NumberFormatException e) {
+			session.setAttribute("viewYear", 0);
+			session.setAttribute("viewMonth", 0);						
+		}
+		return "redirect:/calendar";
+	}
+	
+	@GetMapping("/{yearMonth}/{date}")
+	public String calendarPageWithDateProjectList(@PathVariable("yearMonth") String yearMonth, 
+											@PathVariable("date") String date,
+											RedirectAttributes redirectAttributes) {
+		
+		String fullDate = yearMonth + date;
+		Date dateTypeDate = stringToDate(fullDate);
+		List<ProjectVO> lst = calendarService.getProjectListByDate(dateTypeDate);
+		redirectAttributes.addFlashAttribute("viewDate", date);
 		redirectAttributes.addFlashAttribute("projectListByDate", lst);
-		return "redirect:/calendar";	
+		return "redirect:/calendar";
 	}
 	
 	
--- src/main/webapp/WEB-INF/includes/header.jsp
+++ src/main/webapp/WEB-INF/includes/header.jsp
@@ -3,10 +3,24 @@
 <%@ taglib uri="jakarta.tags.core" prefix="c"%>
 <header>
 	<div class="logo">CompanyCal</div>
-	<div class="view-date">0000년 0월</div>
+	<div class="view-date">${ sessionScope.viewYear }년 ${ sessionScope.viewMonth }월</div>
 	<div class="move-buttons">
-		<button class="prev-button">←</button>
-		<button class="next-button">→</button>
+		<c:choose>
+			<c:when test="${ sessionScope.viewMonth <= 1 }">
+				<a class="prev-button" href="<c:url value='/calendar/${ sessionScope.viewYear - 1 }12' />">←</a>
+			</c:when>
+			<c:otherwise>
+				<a class="prev-button" href="<c:url value='/calendar/${ sessionScope.viewYear }${sessionScope.viewMonth - 1 }' />">←</a>
+			</c:otherwise>
+		</c:choose>
+		<c:choose>
+			<c:when test="${ sessionScope.viewMonth >= 12 }">
+				<a class="next-button" href="<c:url value='/calendar/${ sessionScope.viewYear + 1 }1' />">→</a>
+			</c:when>
+			<c:otherwise>
+				<a class="next-button" href="<c:url value='/calendar/${ sessionScope.viewYear }${sessionScope.viewMonth + 1 }' />">→</a>
+			</c:otherwise>
+		</c:choose>
 	</div>
 	<div class="user-profile">사용자명 ▼</div>
 </header>
