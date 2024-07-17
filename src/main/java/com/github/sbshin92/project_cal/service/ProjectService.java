dd8d5fbbf32f8233bfb40e491686deb49185038b
SBShin92 <aopaoi0987@gmail.com>
2024-07-17 14:25:23 +0900

    fix conflicted files

--- src/main/java/com/github/sbshin92/project_cal/service/ProjectService.java
+++ src/main/java/com/github/sbshin92/project_cal/service/ProjectService.java
@@ -10,7 +10,6 @@ import com.github.sbshin92.project_cal.data.vo.ProjectVO;
 @Service
 public interface ProjectService {
 
-<<<<<<< HEAD
 	List<ProjectVO> getAllProjects();
 	public ProjectVO getProjectById(int projectId);
 	
@@ -33,27 +32,4 @@ public interface ProjectService {
 //
 //    void addFileToProject(ProjectFileVO projectFile, MultipartFile file) throws IOException;
 //	boolean isUserProjectMember(Object currentUserId, int projectId);
-=======
-	 void createProject(ProjectVO project) throws IOException;
-	    ProjectVO getProjectById(int projectId);
-	    List<ProjectVO> getAllProjects();
-	    boolean updateProject(ProjectVO project);
-	    boolean deleteProject(int projectId);	    
-	    boolean isUserProjectMember(Integer userId, Integer projectId); // 프로젝트 멤버확인
-	    List<UserVO> getProjectMembers(Integer projectId); // 프로젝트 멤버 목록 조회
-	    List<ProjectFileVO> getFilesByProjectId(int projectId);
-		void createProjectWithFiles(ProjectVO project, List<MultipartFile> files) throws IOException;
-		void addFileToProject(ProjectFileVO projectFileVO);
-	    void createProject(ProjectVO project, MultipartFile file, List<MultipartFile> files) throws IOException;
-	    void addFileToProject(ProjectFileVO projectFile, MultipartFile file) throws IOException;
-	    ProjectFileVO getFileById(int fileId);
-	    boolean deleteProjectFile(int fileId);
-	    
-	    boolean addProjectUser(int userId, int projectId); // 프로젝트에 유저추가
-	    List<UserVO> getAllUsers();
-//	    List<UserVO> getProjectMembers(int projectId); // 특정 프로젝트의 멤버 목록을 가져옴
-	    List<UserVO> getNonProjectMembers(int projectId); // 특정 프로젝트에 속하지 않은 사용자 목록을 가져옴
-		void deleteProjectMember(int userId, int projectId);
-		
->>>>>>> refs/remotes/origin/develop_minjoon
 }
