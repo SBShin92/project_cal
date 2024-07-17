151e330da29f38816d073a764c65607495969018
SBShin92 <aopaoi0987@gmail.com>
2024-07-17 18:12:13 +0900

    쪽지함 읽은 건지 갯수 확인 추가

--- src/main/java/com/github/sbshin92/project_cal/config/WebMvcConfig.java
+++ src/main/java/com/github/sbshin92/project_cal/config/WebMvcConfig.java
@@ -1,14 +1,20 @@
 package com.github.sbshin92.project_cal.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.github.sbshin92.project_cal.interceptor.AuthInterceptor;
+import com.github.sbshin92.project_cal.interceptor.MessageInterceptor;
+import com.github.sbshin92.project_cal.service.MessageService;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Autowired
+	private MessageService messageService;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new AuthInterceptor())
@@ -16,6 +22,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			.excludePathPatterns("/login")
 			.excludePathPatterns("/css/**")
 			.excludePathPatterns("/javascript/**");
+		
+		registry.addInterceptor(new MessageInterceptor(messageService))
+			.addPathPatterns("/**")
+			.excludePathPatterns("/login")
+			.excludePathPatterns("/css/**")
+			.excludePathPatterns("/javascript/**");
+			
 	}
 
 	
---
+++ src/main/java/com/github/sbshin92/project_cal/interceptor/MessageInterceptor.java
@@ -0,0 +1,42 @@
+package com.github.sbshin92.project_cal.interceptor;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.github.sbshin92.project_cal.data.vo.MessageVO;
+import com.github.sbshin92.project_cal.data.vo.UserVO;
+import com.github.sbshin92.project_cal.service.MessageService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class MessageInterceptor implements HandlerInterceptor {
+
+	private final MessageService messageService;
+	
+	
+	
+	public MessageInterceptor(MessageService messageService) {
+		super();
+		this.messageService = messageService;
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		UserVO authUser = (UserVO)session.getAttribute("authUser");
+		int messageUnreadCount = 0;
+		List<MessageVO> messageVOs = messageService.getMessageListByReceiverUserId(authUser.getUserId());
+		for (MessageVO messageVO: messageVOs) {
+			if ("unread".equals(messageVO.getReadStatus()))
+				messageUnreadCount += 1;
+		}
+		session.setAttribute("messageUnreadCount", messageUnreadCount);
+		return true;
+	}
+}
