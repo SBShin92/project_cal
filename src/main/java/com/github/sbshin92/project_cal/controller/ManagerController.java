eb43e528e6dcc8a6b7552c188899c54ef3cff8b3
yeaeun123 <yeaeun0125@gmail.com>
2024-07-18 15:27:28 +0900

    240718 관리자-사용자 관리 페이지 조회 업데이트

--- src/main/java/com/github/sbshin92/project_cal/controller/ManagerController.java
+++ src/main/java/com/github/sbshin92/project_cal/controller/ManagerController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.github.sbshin92.project_cal.data.vo.ProjectVO;
+import com.github.sbshin92.project_cal.data.vo.UserVO;
 import com.github.sbshin92.project_cal.service.ProjectService;
+import com.github.sbshin92.project_cal.service.UserService;
 
 @RequestMapping("/manager")
 @Controller
@@ -19,6 +21,8 @@ public class ManagerController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping({"", "/", "/projects"})
 	public String managerProjectsPage(Model model, @RequestParam(defaultValue = "1") int page) {
@@ -31,10 +35,9 @@ public class ManagerController {
 	
 	@GetMapping({"/users"})
 	public String managerUsersPage(Model model) {
-		
-		
-		
-//	        model.addAttribute("roles", roleService.getAllRoles());
+		List<UserVO> userVOs = userService.getAllUsers();
+		model.addAttribute("userVOs", userVOs);
+
 		return "manager/manager-users";
 	}
 	
